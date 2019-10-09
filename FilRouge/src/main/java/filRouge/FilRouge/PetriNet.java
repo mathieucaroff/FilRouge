e68f5973afaceb567790ae5f8cf16533735aa23e
Youba TARAZA <youbataraza@gmail.com>
2019-10-09 15:21:07 +0200

    create PetriNet

--- FilRouge/src/main/java/filRouge/FilRouge/PetriNet.java
+++ FilRouge/src/main/java/filRouge/FilRouge/PetriNet.java
@@ -1,6 +1,18 @@
 package filRouge.FilRouge;
 
-public interface PetriNet {
-	
+import java.io.IOException;
 
+public interface PetriNet {
+	Place createPlace();
+	Transition createTransition();
+	ArcPull createArcPullMultiplicity(Place p, Transition t,int c);
+	ArcPull createArcPullZero(Place p, Transition t);
+	ArcPull createArcPullVacuum(Place p, Transition t);
+	ArcPush createArcPush(Place p, Transition t, int c);
+	void deleteArcPull(ArcPull arcpull);
+	void deleteArcPush(ArcPush arcpush);
+	void deletPlace(Place p);
+	void deleteTransition(Transition t);
+	void singleStep() throws RuntimeException  ;
+	int loopStep();
 }
